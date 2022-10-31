35b7bf6db6d41bcf235ad1b87a7258a149bdd8b8
heba-ismaeel <heba.m.ismaeel@gmail.com>
2022-10-31 16:26:22 +0200

    update

--- src/components/Features/popup/index.js
+++ src/components/Features/popup/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-modal";
 import Image from "./image";
 import style from "./style.module.css";
@@ -7,6 +7,7 @@ import data from "../data.json";
 
 export const PopUp = ({ modalIsOpen, updatePopUp ,product}) => {
   const {title,price,images,thumbnail,rating}=product
+  const [rightImage, setRightImage]=useState({thumbnail})
   
   return (
     <Modal
@@ -23,27 +24,32 @@ export const PopUp = ({ modalIsOpen, updatePopUp ,product}) => {
       <div className={style.wrapper}>
         <div className={style.title}>
           <h5>{title}</h5>
-          <i class="fal fa-times fa-2x" onClick={updatePopUp}>
+          <button class="fal fa-times fa-2x"  onClick={updatePopUp}>
             {" "}
-          </i>
+          </button>
         </div>
       </div>
       <div className={style.main}>
         <div className={style.left}>
-          <Image images={images} />
-          <div className={style.imgright}>
-            <img
-              src={thumbnail}
-              alt=""
-            />
-          </div>
+          <Image images={images} thumbnail={thumbnail}  />
         </div>
         <div className={style.right}>
           <div className={style.container}>
             <h5 className={style.title}>{title} </h5>
             <div className={style.heading}>
               <h5 className={style.price}>{price}.00$ </h5>
+              <div className={style.theWrapper}>
+              <div className={style.rating}>
+                  <a href="#5" title="Give 5 stars">★</a>
+                  <a href="#4" title="Give 4 stars">★</a>
+                  <a href="#3" title="Give 3 stars">★</a>
+                  <a href="#2" title="Give 2 stars">★</a>
+                  <a href="#1" title="Give 1 star">★</a>
+                
+                </div>
               <h5 className={style.rating}> {rating} of 5</h5>
+              </div>
+              
             </div>
             <hr />
 
