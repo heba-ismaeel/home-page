5551df3f5c6b1ae2e90b49234a7b2d2d25a758d0
heba-ismaeel <heba.m.ismaeel@gmail.com>
2022-12-20 15:26:14 +0200

    recommended is done

--- src/components/Recominded/Vedio/index.js
+++ src/components/Recominded/Vedio/index.js
@@ -1,34 +1,68 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import styles from "./styles.module.css";
+import { BiPlayCircle } from "react-icons/bi";
+import { RiPlayCircleLine } from "react-icons/ri";
+const arr=[
+  ['assests/images/635c0a53670aa9b611314996afb28e7cbfb6a4ec.png',"Jamdani"],
+  ['assests/images/2ef8f1a6888f5e313706fce9ac7b0c9d436b5104.png' ,"Lorem ipsum dolor sit amet"],
+  ['assests/images/2f06e2ebbe92b4a0b18863c9322dba5d691c54fc.png' ,"Lorem ipsum dolor sit amet"]
 
- import { RiPlayCircleLine } from "react-icons/ri";
-
+];
+const recommendedData = [
+  {
+    id: 0,
+    title: "Jamdani",
+    imgSrc: "/assets/images/RecommendedVideos/video1.png",
+  },
+  {
+    id: 1,
+    title: "Lorem ipsum dolor sit amet",
+    imgSrc: "/assets/images/RecommendedVideos/video2.png",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum dolor sit amet",
+    imgSrc: "/assets/images/RecommendedVideos/video3.png",
+  },
+];
 
 const Vedio = () => {
-  const vedio=[
-    ['assests/images/635c0a53670aa9b611314996afb28e7cbfb6a4ec.png',"Jamdani"],
-    ['assests/images/2ef8f1a6888f5e313706fce9ac7b0c9d436b5104.png' ,"Lorem ipsum dolor sit amet"],
-    ['assests/images/2f06e2ebbe92b4a0b18863c9322dba5d691c54fc.png' ,"Lorem ipsum dolor sit amet"]
-
-  ];
   return (
     <>
-    
- <div className={styles.bigcont}>
-{vedio.map((item)=>
-  <div className={styles.container}>
- <img src={item[0]} className={styles.img}></img>
- <center>
- <RiPlayCircleLine  className={styles.icon}/></center>
-<div className={styles.par}> {item[1]}</div>
-    </div>  
-)}
-
-</div>
-
-
-    
-  </>
-  
+      <Container className={styles.RecVideosContainer}>
+        <Row>
+          {arr.map((video) => {
+            return (
+              <Col
+                xl="4"
+                lg="4"
+                md="4"
+                sm="4"
+                key={video.id}
+                className={styles.video}
+              >
+                <img
+                  className={styles.videoImg}
+                  src={video[0]}
+                  alt={video[1]}
+                />
+                <p className={styles.par}>{video[1]}</p>
+                <div className={styles.overlay}>
+                  {/* <BiPlayCircle    color="white" className={styles.playIcon} /> */}
+                  <RiPlayCircleLine color="white" className={styles.playIcon}/>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+        <Row>
+         
+        </Row>
+      </Container>
+    </>
   );
 };
 
